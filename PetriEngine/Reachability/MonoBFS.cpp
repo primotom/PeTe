ff0d2ad82b1998decc3bfb3be1907ad0abcd6e70
Thomas Nielsen <primogens@gmail.com>
2011-12-06 11:24:04 +0100

    Cleaned up strategies.. Old BestFS heuristic should work

--- PetriEngine/Reachability/MonoBFS.cpp
+++ PetriEngine/Reachability/MonoBFS.cpp
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "MonoBFS.h".h"
+#include "MonoBFS.h"
 #include "../PQL/PQL.h"
 #include "../PQL/Contexts.h"
 #include "../Structures/StateSet.h"
