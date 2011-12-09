b919451addefe8cdfb52688b9519445d22b99fc9
Karsten <Karstenjjakobsen@gmail.com>
2011-12-09 14:11:56 +0100

    working on random

---
+++ PetriEngine/Structures/RandomDFSStateset.cpp
@@ -0,0 +1,47 @@
+#include "RandomDFSStateset.h"
+
+
+bool RandomDFSStateSet::add(State *state){
+	// Ensure we're larger than any state in waiting. Pop any states smaller
+	bool skipVisited = false;
+	for(iter it = _waiting.begin(); it != _waiting.end(); it++){
+		if(this->less(*it, state)){
+			_waiting.remove(*it++);
+			skipVisited = true;
+		} else {
+			if(this->leq(state, *it))
+				return false;
+			it++;
+		}
+	}
+
+	// Ensure we haven't been visited, or are smaller than or
+	//  equal any states in visited
+	if(!skipVisited){
+		for(iter it = _visited.begin(); it != _visited.end(); it++){
+			if(this->leq(state, *it))
+				return false;
+			it++;
+		}
+	}
+
+
+	return true;
+}
+
+void RandomDFSStateSet::pushToWating(State* state){
+	_waiting.push_back(state);
+}
+
+State* RandomDFSStateSet::getNextState(){
+	if(!_waiting.empty()){
+		State* retState = _waiting.back();
+		_waiting.pop_back();
+		_visited.push_back(retState);
+		return retState;
+	} else return NULL;
+}
+
+size_t RandomDFSStateSet::waitingSize(){
+	return _waiting.size();
+}
---
+++ PetriEngine/Structures/RandomDFSStateset.h
@@ -0,0 +1,41 @@
+#ifndef RANDOMDFSSTATESET_H
+#define RANDOMDFSSTATESET_H
+
+#include "PQL/Contexts.h"
+#include "OrderableStateSet.h"
+#include <list>
+
+namespace PetriEngine { namespace Structures {
+
+/** Orderable State Set class which will feed states in a Breadth First manner */
+class RandomDFSStateSet : public OrderableStateSet {
+public:
+	/** Create the state set */
+	RandomDFSStateSet(const PetriNet& net, PQL::MonotonicityContext* context)
+		: OrderableStateSet(net, context) {}
+
+	/** Add a state to the state set */
+	bool add(State* state);
+
+	/** Get the next state to explore */
+	State* getNextState();
+
+	/** Get size of waiting */
+	size_t waitingSize();
+
+
+	void pushToWating(State* state);
+
+private:
+	typedef std::list<State*>::iterator iter;
+
+	std::list<State*> _waiting;
+	std::list<State*> _visited;
+
+
+};
+
+} // Structures
+} // PetriEngine
+
+#endif // RANDOMDFSSTATESET_H
