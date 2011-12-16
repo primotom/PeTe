f40ab35d807caa5b8bd13dcd2032150e249a2ae8
Karsten <Karstenjjakobsen@gmail.com>
2011-12-16 19:02:31 +0100

    redone the testtable.py

--- createTestTable.py
+++ createTestTable.py
@@ -1,8 +1,5 @@
 f = open("SuperNewTests.csv")
 
-
-
-
 curCurry = ""
 model = ""
 method = ""
@@ -23,26 +20,26 @@ for line in f:
 			if (i > 1):
 				print "\\end{tabular}"
 			print ""
-			print "\\begin{tabular}{|l|l|c|c|c|c|c|c|c|}\hline"
-			print "\multicolumn{9}{|c|}{" + lSp[1] + " with " + lSp[2] +  " } \\\\\\hline"
-			print " Model & Satisfied & Time  & Expanded & Explored & Trace & Transitions Fired & Removed & Memory \\\\\\hline"
+			print "\\begin{tabular}{|l|l|c|c|c|c|c|c|}\hline"
+			print "\multicolumn{8}{|c|}{" + lSp[1] + " with " + lSp[2] +  " } \\\\\\hline"
+			print " Model & Satisfied & Time  & Expanded & Explored & Trace & Fired & Removed  \\\\\\hline"
 		sat = "N/A"
 
 		if( lSp[3].find("Not satisfiable") >= 0):
-			sat = "NO"
-			print lSp[0].strip(".pet") + " & " + sat + " & " + lSp[4]+ " & " + lSp[5] + " & " + lSp[6] + " & " + lSp[7] + " & " + lSp[8]  + " & " + lSp[9]  + " & " + lSp[10].strip("\n")  + "\\\\\\hline"
+			sat = "No"
+			print lSp[0].strip(".pet") + " & " + sat + " & " + lSp[4]+ " & " + lSp[5] + " & " + lSp[6] + " & " + lSp[7] + " & " + lSp[8]  + " & " + lSp[9] + "\\\\\\hline"
 		
 		if( lSp[3].find("Satisfied") >= 0):
 			sat = "YES"
-			print lSp[0].strip(".pet") + " & " + sat + " & " + lSp[4]+ " & " + lSp[5] + " & " + lSp[6] + " & " + lSp[7] + " & " + lSp[8]  + " & " + lSp[9]  + " & " + lSp[10].strip("\n")  + "\\\\\\hline"
+			print lSp[0].strip(".pet") + " & " + sat + " & " + lSp[4]+ " & " + lSp[5] + " & " + lSp[6] + " & " + lSp[7] + " & " + lSp[8]  + " & " + lSp[9] + "\\\\\\hline"
 		
 		if( lSp[3].find("OutOfTime") >= 0):
-			sat = "Out Of Time"
-			print lSp[0].strip(".pet") + " & " + sat + " & - & - & - & - & - & - & " + lSp[8].strip("\n")  + "\\\\\\hline"
+			sat = "Out of time"
+			print lSp[0].strip(".pet") + " & " + sat + " & - & - & - & - & - & - \\\\\\hline"
 	
 		if( lSp[3].find("OutOfMemory") >= 0):
-			sat = "Out Of Memory"
-			print lSp[0].strip(".pet") + " & " + sat + " & - & - & - & - & - & - & " + lSp[8].strip("\n")  + "\\\\\\hline"
+			sat = "Out of memory"
+			print lSp[0].strip(".pet") + " & " + sat + " & - & - & - & - & - & - \\\\\\hline"
 
 		
 		
